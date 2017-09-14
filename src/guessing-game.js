203a8d43e96e1cf63f30dc5d37bed7bf6966737a
blohamen <staver82@gmail.com>
2017-09-14 23:14:28 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.arr = [];
+    	this.index = 0;
+    	this.min = 0,
+    	this.max = 0;
+    }
 
     setRange(min, max) {
-
+		for(var i = min; i <= max; i++){
+			this.arr[i] = i;
+		}	
+		this.min = min;
+		this.max = max;
+		this.index = Math.round((min + max)/2);
     }
 
     guess() {
-
+    	return this.arr[this.index];
     }
 
     lower() {
-
+    	this.max= this.index;
+    	this.index = Math.round((this.max + this.min)/2);
     }
 
     greater() {
-
+    	this.min= this.index;
+    	this.index = Math.round((this.max + this.min)/2);
     }
 }
 
